00ee67801b6aa23fd6eb4f5b8dc57176f39b9525
Thiago Faria de Andrade <thiagofa@gmail.com>
2018-04-14 16:51:05 -0300

    Descrições dos codigos dos metodos de pagamentos adicionadas

--- source/src/main/java/br/com/uol/pagseguro/api/common/domain/TransactionPaymentMethod.java
+++ source/src/main/java/br/com/uol/pagseguro/api/common/domain/TransactionPaymentMethod.java
@@ -63,44 +63,44 @@ public interface TransactionPaymentMethod {
    */
   enum Code {
 
-    CREDIT_CARD_VISA(101),
-    CREDIT_CARD_MASTERCARD(102),
-    CREDIT_CARD_AMEX(103),
-    CREDIT_CARD_DINERS(104),
-    CREDIT_CARD_HIPERCARD(105),
-    CREDIT_CARD_AURA(106),
-    CREDIT_CARD_ELO(107),
-    CREDIT_CARD_PLENOCARD(108),
-    CREDIT_CARD_PERSONALCARD(109),
-    CREDIT_CARD_JCB(110),
-    CREDIT_CARD_DISCOVER(111),
-    CREDIT_CARD_BRASILCARD(112),
-    CREDIT_CARD_FORTBRASIL(113),
-    CREDIT_CARD_CARDBAN(114),
-    CREDIT_CARD_VALECARD(115),
-    CREDIT_CARD_CABAL(116),
-    CREDIT_CARD_MAIS(117),
-    CREDIT_CARD_AVISTA(118),
-    CREDIT_CARD_GRANDCARD(119),
-    CREDIT_CARD_SOROCRED(120),
+    CREDIT_CARD_VISA(101, "Cartão de crédito Visa"),
+    CREDIT_CARD_MASTERCARD(102, "Cartão de crédito MasterCard"),
+    CREDIT_CARD_AMEX(103, "Cartão de crédito American Express"),
+    CREDIT_CARD_DINERS(104, "Cartão de crédito Diners"),
+    CREDIT_CARD_HIPERCARD(105, "Cartão de crédito Hipercard"),
+    CREDIT_CARD_AURA(106, "Cartão de crédito Aura"),
+    CREDIT_CARD_ELO(107, "Cartão de crédito Elo"),
+    CREDIT_CARD_PLENOCARD(108, "Cartão de crédito PLENOCard"),
+    CREDIT_CARD_PERSONALCARD(109, "Cartão de crédito PersonalCard"),
+    CREDIT_CARD_JCB(110, "Cartão de crédito JCB"),
+    CREDIT_CARD_DISCOVER(111, "Cartão de crédito Discover"),
+    CREDIT_CARD_BRASILCARD(112, "Cartão de crédito BrasilCard"),
+    CREDIT_CARD_FORTBRASIL(113, "Cartão de crédito FortBrasil"),
+    CREDIT_CARD_CARDBAN(114, "Cartão de crédito CardBan"),
+    CREDIT_CARD_VALECARD(115, "Cartão de crédito ValeCard"),
+    CREDIT_CARD_CABAL(116, "Cartão de crédito Cabal"),
+    CREDIT_CARD_MAIS(117, "Cartão de crédito Mais!"),
+    CREDIT_CARD_AVISTA(118, "Cartão de Crédito Avista"),
+    CREDIT_CARD_GRANDCARD(119, "Cartão de crédito GrandCard"),
+    CREDIT_CARD_SOROCRED(120, "Cartão de crédito Sorocred"),
 
-    BOLETO_BRADESCO(201),
-    BOLETO_SANTANDER(202),
+    BOLETO_BRADESCO(201, "Boleto Bradesco"),
+    BOLETO_SANTANDER(202, "Boleto Santander"),
 
-    ONLINE_DEBIT_BRADESCO(301),
-    ONLINE_DEBIT_ITAU(302),
-    ONLINE_DEBIT_UNIBANCO(303),
-    ONLINE_DEBIT_BANCO_BRASIL(304),
-    ONLINE_DEBIT_BANCO_REAL(305),
-    ONLINE_DEBIT_BANRISUL(306),
-    ONLINE_DEBIT_HSBC(307),
+    ONLINE_DEBIT_BRADESCO(301, "Débito online Bradesco"),
+    ONLINE_DEBIT_ITAU(302, "Débito online Itaú"),
+    ONLINE_DEBIT_UNIBANCO(303, "Débito online Unibanco"),
+    ONLINE_DEBIT_BANCO_BRASIL(304, "Débito online Branco do Brasil"),
+    ONLINE_DEBIT_BANCO_REAL(305, "Débito online Banco Real"),
+    ONLINE_DEBIT_BANRISUL(306, "Débito online Banrisul"),
+    ONLINE_DEBIT_HSBC(307, "Débito online HSBC"),
 
-    PAGSEGURO_BALANCE(401),
+    PAGSEGURO_BALANCE(401, "Saldo PagSeguro"),
 
-    OI_PAGGO(501),
+    OI_PAGGO(501, "Oi Paggo"),
 
-    BANK_DEPOSIT_BANCO_BRASIL(701),
-    BANK_DEPOSIT_HSBC(702),
+    BANK_DEPOSIT_BANCO_BRASIL(701, "Depósito em conta - Banco do Brasil"),
+    BANK_DEPOSIT_HSBC(702, "Depósito em conta - HSBC"),
 
 
     /**
@@ -108,22 +108,28 @@ public interface TransactionPaymentMethod {
      *
      * UNKNOW
      */
-    UNKNOW(null),
+    UNKNOW(null, "Não informado"),
 
     /**
      * Used when you have the code but is not mapped in lib.
      *
      * UNRECOGNIZED
      */
-    UNRECOGNIZED(null);
+    UNRECOGNIZED(null, "Não reconhecido");
 
     private Integer codeId;
+    private String description;
 
     /**
      * Constructor
      */
-    Code(Integer codeId) {
+    Code(Integer codeId, String description) {
       this.codeId = codeId;
+      this.description = description;
+    }
+
+    public String getDescription() {
+      return description;
     }
 
     /**
